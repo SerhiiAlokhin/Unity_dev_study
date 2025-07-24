f586dd185916d09188cc28eab8f0ab4558a5657a
SerhiiAlokhin <serega.alokhin@gmail.com>
2025-07-24 23:58:02 +0300

    add 5 cinematic cameras and refactor ball movement

--- Assets/ball_movement.cs
+++ Assets/ball_movement.cs
@@ -1,22 +1,39 @@
 using UnityEngine;
+using Cinemachine;
 
 public class PlayerBall : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 5f;
 
     private Rigidbody rb;
+    private CinemachineBrain brain;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
+        brain = Camera.main.GetComponent<CinemachineBrain>();
     }
 
     private void Update()
     {
-        float moveX = Input.GetAxis("Horizontal");
-        float moveZ = Input.GetAxis("Vertical");   
+        if (brain == null || brain.ActiveVirtualCamera == null)
+            return;
 
-        Vector3 movement = new Vector3(moveX, 0f, moveZ);
-        rb.AddForce(movement * moveSpeed);
+        Transform camTransform = brain.OutputCamera.transform;
+
+        float moveX = Input.GetAxis("Horizontal");
+        float moveZ = Input.GetAxis("Vertical");
+
+        Vector3 camForward = camTransform.forward;
+        Vector3 camRight = camTransform.right;
+
+        camForward.y = 0f;
+        camRight.y = 0f;
+
+        camForward.Normalize();
+        camRight.Normalize();
+
+        Vector3 moveDir = camForward * moveZ + camRight * moveX;
+        rb.AddForce(moveDir * moveSpeed);
     }
 }
