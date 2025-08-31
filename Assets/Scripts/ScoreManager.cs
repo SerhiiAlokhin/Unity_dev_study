1dcbaebe43459590c232e9a4996b11568cbc7429
SerhiiAlokhin <serega.alokhin@gmail.com>
2025-08-31 23:32:13 +0300

    Player settings saving and saving count of rings

--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -4,29 +4,30 @@ using TMPro;
 public class ScoreManager : MonoBehaviour
 {
     public static ScoreManager Instance { get; private set; }
-
     [SerializeField] private TextMeshProUGUI ringsText;
-    private int rings;
+    private RingsData data;
 
     private void Awake()
     {
-        if (Instance != null && Instance != this)
-        {
-            Destroy(gameObject);
-            return;
-        }
+        if (Instance != null && Instance != this) { Destroy(gameObject); return; }
         Instance = this;
+        DontDestroyOnLoad(gameObject);
+
+        data = GameSaver.TryLoad<RingsData>(out var loaded) ? loaded : new RingsData();
+        //Debug.Log($"[Rings] loaded total={data.totalRings} (from file)");
         UpdateUI();
     }
 
     public void AddRing(int amount = 1)
     {
-        rings += amount;
+        data.totalRings += amount;
+        GameSaver.Save(data);
+        //Debug.Log($"[Rings] +{amount} => {data.totalRings} (saved)");
         UpdateUI();
     }
 
     private void UpdateUI()
     {
-        if (ringsText) ringsText.text = "x" + rings.ToString();
+        if (ringsText) ringsText.text = "x" + data.totalRings;
     }
 }
