ef0636cae28a3170273fa9fba9c017eb140aceac
SerhiiAlokhin <serega.alokhin@gmail.com>
2025-09-03 00:20:42 +0300

    change saving rings

--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -4,30 +4,64 @@ using TMPro;
 public class ScoreManager : MonoBehaviour
 {
     public static ScoreManager Instance { get; private set; }
+
     [SerializeField] private TextMeshProUGUI ringsText;
+    [SerializeField] private int autosaveEveryRings = 10;
+
+
     private RingsData data;
+    private int unsavedDelta;   
+    private bool valueChanged;
 
     private void Awake()
     {
-        if (Instance != null && Instance != this) { Destroy(gameObject); return; }
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
         Instance = this;
-        DontDestroyOnLoad(gameObject);
 
         data = GameSaver.TryLoad<RingsData>(out var loaded) ? loaded : new RingsData();
-        //Debug.Log($"[Rings] loaded total={data.totalRings} (from file)");
         UpdateUI();
     }
 
     public void AddRing(int amount = 1)
     {
         data.totalRings += amount;
-        GameSaver.Save(data);
-        //Debug.Log($"[Rings] +{amount} => {data.totalRings} (saved)");
+        valueChanged = true;
+        unsavedDelta += amount;
+
+        if (autosaveEveryRings > 0 && unsavedDelta >= autosaveEveryRings)
+            SaveNow();
+
         UpdateUI();
+    }
+
+    public void SaveNow()
+    {
+        if (!valueChanged) return;
+        GameSaver.Save(data);
+        valueChanged = false;
+        unsavedDelta = 0;
     }
 
     private void UpdateUI()
     {
         if (ringsText) ringsText.text = "x" + data.totalRings;
+    }
+
+    private void OnApplicationPause(bool pause)
+    {
+        if (pause)
+        {
+            SaveNow();
+        }
+    }
+
+    private void OnApplicationQuit()
+    {
+        SaveNow();
     }
 }
